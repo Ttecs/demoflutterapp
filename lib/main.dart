e66a401ded2d3cfada1ffea050f49533a98abde7
Ttecs <tharakahalkewelatecs@gmail.com>
2021-04-12 19:17:56 +0530

    buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,43 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(App());
+}
 
-class MyApp extends StatelessWidget {
+class App extends StatelessWidget {
+  void awnserQuectons() {
+    print('awnser choosen');
+  }
+
+  @override
   Widget build(BuildContext context) {
+    var quections = [
+      'what is  your color',
+      'what is your age',
+      'what is your home'
+    ];
+    // TODO: implement build
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(title: Text('My first  App')),       
-          body: Text('This is my text')) ,       
-		    appBar: AppBar(title: Text('My first  App')),       
-          body: Text('This is my text')) ,
-    );   
+        appBar: AppBar(
+          title: Text("my app 2"),
+        ),
+        body: Column(children: [
+          Text("The Quection"),
+          RaisedButton(
+            child: Text("Awnser 1"),
+            onPressed: awnserQuectons,
+          ),
+          RaisedButton(
+            child: Text("Awnser 2"),
+            onPressed: awnserQuectons,
+          ),
+          RaisedButton(
+            child: Text("Awnser 3"),
+            onPressed: awnserQuectons,
+          ),
+        ]),
+      ),
+    );
   }
 }
-   
